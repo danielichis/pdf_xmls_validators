5398bdf9d077213efab82c4d494731275754f85a
Danielichis <dchaconb@uni.pe>
2022-05-12 18:37:25 -0500

    campos completados resta comparar y validar

--- parsePdf.py
+++ parsePdf.py
@@ -1,22 +1,50 @@
 import pandas as pd
+import datetime
+import locale
 import pdfplumber
 import re
+
+def parse_date(entireText):
+  locale.setlocale(locale.LC_TIME, "es_ES")
+  day=re.findall(r"Fecha de emisión  (\d+)  de  ([a-zA-z]+)  del  (\d+)",entireText,flags=re.I)[0][0]
+  month=re.findall(r"Fecha de emisión  (\d+)  de  ([a-zA-z]+)  del  (\d+)",entireText,flags=re.I)[0][1]
+  year=re.findall(r"Fecha de emisión  (\d+)  de  ([a-zA-z]+)  del  (\d+)",entireText,flags=re.I)[0][2]
+  fecha=f"{day}-{month}-{year}"
+  t = datetime.datetime.strptime(fecha, "%d-%B-%Y").date()
+  print(t)
+  return t
 def pdff(ruta):
   with pdfplumber.open(ruta) as temp:
     print(f"validando ruta:...{ruta}")
     first_page = temp.pages[0]
     entireText=first_page.extract_text()
-    print(entireText)
-    nameSupplier=re.findall(r"(\A.*)",entireText,flags=re.I)[0]
-    dniSupplier=re.findall(r"R\.U\.C\.  (\d+)",entireText,flags=re.I)[0]
-    priceAmount=re.findall(r"Total Neto Recibido: (\d+\.\d+) SOLES",entireText,flags=re.I)[0]
-    nameFile=re.findall(r"(\d{9,}.+)\.",ruta)[0]
+    #print(entireText)
+  nameFile=re.findall(r"(\d{9,}.+)\.",ruta)[0]
+  rucSupplier=re.findall(r"R\.U\.C\.  (\d+)",entireText,flags=re.I)[0]
+  nameSupplier=re.findall(r"(\A.*)",entireText,flags=re.I)[0]
+  totalPriceAmount=re.findall(r"Total por honorarios:  (\d+.\d+)",entireText,flags=re.I)[0]
+  emissionDate=str("'"+str(parse_date(entireText)))
+  rucClient=re.findall(r"Identificado con  RUC  número  (\d+)",entireText,flags=re.I)[0]
+  nameClient=re.findall(r"Recibí de:  (.+)",entireText,flags=re.I)[0]
+  lettersAmount=re.findall(r"La suma de:  (.+) SOLES",entireText,flags=re.I)[0]
+  serialNumber=re.findall(r"Nro: (E\d{3})- \d+",entireText,flags=re.I)[0]
+  netPriceAmount=re.findall(r"Total Neto Recibido: (\d+\.\d+) SOLES",entireText,flags=re.I)[0]
+  vouchNumber=re.findall(r"_(\d+).pdf",ruta)[0]
+  typeDocument=re.findall(r"_(\d+)_",ruta)[0]
   supplierInfoList=[]
   supplierInfo={
-      'nameFile':nameFile,
-      'DNI':dniSupplier,
-      'NOMBRES':nameSupplier,
-      'PRECIO_SERVICIO':priceAmount,
+        'nameFile':nameFile,
+        'RUC EMISOR':rucSupplier,
+        'NOMBRE EMISOR':nameSupplier,
+        'PRECIO TOTAL BRUTO':totalPriceAmount,
+        'PRECIO TOTAL NETO':netPriceAmount,
+        'FECHA DE EMISION':emissionDate,
+        'RUC RECEPTOR':rucClient,
+        'NOMBRE RECEPTOR':nameClient,
+        'PRECIO LETRAS':lettersAmount,
+        'NUMERO COMPROBATE':vouchNumber,
+        'SERIE':serialNumber,
+        'TIPO DOCUMENTO':typeDocument,
   }
   supplierInfoList.append(supplierInfo)
   return supplierInfo
@@ -24,3 +52,4 @@ def pdff(ruta):
 #print(df)
 print("imprimiendo fuera de la funcion")
 print(pdff(r'C:/Users/LENOVO/Documents/RobotDaniel/XML PDF PARSER/pdfsFolder/10480238011_02_E001_20.pdf'))
+#parse_date()
--- parseXml.py
+++ parseXml.py
@@ -4,20 +4,38 @@ import re
 def xmll(ruta):
     with open(ruta) as page:
         soup = bs4.BeautifulSoup(page, 'xml')
-    suplierOb=soup.find('cac:AccountingSupplierParty')
-    dniSupplier=suplierOb.find('cbc:CustomerAssignedAccountID').text
-    nameSupplier=suplierOb.find('cbc:Name').text.strip()
-    priceAmount=soup.find('cbc:PriceAmount').text
     nameFile=re.findall(r"(\d{9,}.+)\.",ruta)[0]
+    suplierOb=soup.find('cac:AccountingSupplierParty')
+    rucSupplier=suplierOb.find('cbc:CustomerAssignedAccountID').text
+    nameSupplier=suplierOb.find('cbc:Name').text.strip()
+    totalPriceAmount=soup.find('cbc:PayableAmount').text
+    netPriceAmount=soup.find('cbc:PriceAmount').text
+    emissionDate=soup.find('cbc:IssueDate').text
+    vouchNumber=re.findall(r"_(\d+).xml",ruta)[0]
+    lettersAmount=soup.find('cbc:Note').text
+    serialNumber=soup.find('cac:OrderReference').find('cbc:ID').text
+    rucClient=soup.find('cac:AccountingCustomerParty').find('cbc:CustomerAssignedAccountID').text
+    nameClient=soup.find('cac:AccountingCustomerParty').find('cac:Party').find('cac:PartyName').find('cbc:Name').text.strip()
+    typeDocument=re.findall(r"_(\d+)_",ruta)[0]
+   
     supplierInfoList=[]
     supplierInfo={
         'nameFile':nameFile,
-        'DNI':dniSupplier,
-        'NOMBRES':nameSupplier,
-        'PRECIO_SERVICIO':priceAmount,
+        'RUC EMISOR':rucSupplier,
+        'NOMBRE EMISOR':nameSupplier,
+        'PRECIO TOTAL BRUTO':totalPriceAmount,
+        'PRECIO TOTAL NETO':netPriceAmount,
+        'FECHA DE EMISION':emissionDate,
+        'RUC RECEPTOR':rucClient,
+        'NOMBRE RECEPTOR':nameClient,
+        'PRECIO LETRAS':lettersAmount,
+        'NUMERO COMPROBATE':vouchNumber,
+        'SERIE':serialNumber,
+        'TIPO DOCUMENTO':typeDocument,
     }
     supplierInfoList.append(supplierInfo)
     return supplierInfo
 #df=pd.DataFrame(list(supplierInfoList))
 #print(df)
+print(xmll(r"xmlFolder\10480238011_02_E001_20.xml"))
 
